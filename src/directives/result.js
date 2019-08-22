337ef0f7ec9fb1c01c4a1d60c8dad318c9016535
Raychee Zhang <Raychee.Zhang@gmail.com>
2019-08-22 14:57:25 +0800

    0.3.25 add compatibility for custom resolved result objects

--- src/directives/result.js
+++ src/directives/result.js
@@ -1,59 +1,9 @@
 const {SchemaDirectiveVisitor} = require('graphql-tools');
-const {getNamedType, GraphQLInt, GraphQLObjectType} = require('graphql');
+const {getNamedType, defaultFieldResolver, GraphQLInt, GraphQLObjectType} = require('graphql');
 const {GraphQLJSONObject} = require('graphql-type-json');
 
 const config = require('../config');
 const {sideCar} = require('../utils');
-
-
-function ensureResultType(schema, fieldType) {
-    const typeName = getNamedType(fieldType).name;
-    const resultTypeName = `${typeName}${config.FIELD_PREFIX_RESULT}`;
-    let resultType = schema.getType(resultTypeName);
-    if (!resultType) {
-        resultType = new GraphQLObjectType({
-            name: resultTypeName,
-            fields: {
-                results: {
-                    type: fieldType,
-                    async resolve(parent) {
-                        return await parent.getResults();
-                    }
-                },
-                count: {
-                    type: GraphQLInt,
-                    async resolve(parent) {
-                        return await parent.getCount();
-                    }
-                },
-                debug: {
-                    type: GraphQLJSONObject,
-                    async resolve(parent) {
-                        return await parent.getDebugInfo();
-                    }
-                }
-            }
-        });
-        resultType._augmentType = 'result.type';
-        resultType._augmentedTypeName = typeName;
-        resultType._augmentedMode = config.MODE_RESULT;
-        schema.getTypeMap()[resultTypeName] = resultType;
-    }
-    return resultType;
-}
-
-
-class Result extends SchemaDirectiveVisitor {
-
-    visitFieldDefinition(field, details) {
-        if (details.objectType === this.schema.getQueryType()) {
-            if (field.type._augmentType !== 'result.type') {
-                field.type = ensureResultType(this.schema, field.type);
-            }
-        }
-    }
-
-}
 
 
 class ResultResolver {
@@ -105,6 +55,69 @@ class ResultResolver {
             }
         }
         return this._debug;
+    }
+
+}
+
+
+function ensureResultType(schema, fieldType) {
+    const typeName = getNamedType(fieldType).name;
+    const resultTypeName = `${typeName}${config.FIELD_PREFIX_RESULT}`;
+    let resultType = schema.getType(resultTypeName);
+    if (!resultType) {
+        resultType = new GraphQLObjectType({
+            name: resultTypeName,
+            fields: {
+                results: {
+                    type: fieldType,
+                    async resolve(...args) {
+                        if (args[0] instanceof ResultResolver) {
+                            return await args[0].getResults();
+                        } else {
+                            return defaultFieldResolver.apply(this, args);
+                        }
+                    }
+                },
+                count: {
+                    type: GraphQLInt,
+                    async resolve(...args) {
+                        if (args[0] instanceof ResultResolver) {
+                            return await args[0].getCount();
+                        } else {
+                            return defaultFieldResolver.apply(this, args);
+                        }
+                    }
+                },
+                debug: {
+                    type: GraphQLJSONObject,
+                    async resolve(...args) {
+                        if (args[0] instanceof ResultResolver) {
+                            return await args[0].getDebugInfo();
+                        } else {
+                            return defaultFieldResolver.apply(this, args);
+                        }
+                    }
+                }
+            }
+        });
+        resultType._augmentType = 'result.type';
+        resultType._augmentedTypeName = typeName;
+        resultType._augmentedMode = config.MODE_RESULT;
+        schema.getTypeMap()[resultTypeName] = resultType;
+    }
+    return resultType;
+}
+
+
+class Result extends SchemaDirectiveVisitor {
+
+    visitFieldDefinition(field, details) {
+        if (details.objectType !== this.schema.getQueryType() && details.objectType !== this.schema.getMutationType()) {
+            throw new Error(`directive "@${config.MODE_RESULT}" should be only used on root query/mutation fields`);
+        }
+        if (field.type._augmentType !== 'result.type') {
+            field.type = ensureResultType(this.schema, field.type);
+        }
     }
 
 }
