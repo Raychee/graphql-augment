bbd62b675489201ec39d594ff6b8217b3792c06d
Raychee <raychee.zhang@gmail.com>
2020-10-10 13:34:23 +0800

    0.9.0: refactor all directives into the form of @{mode} with page, sort, etc. as args

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const {GraphQLDateTime} = require('graphql-iso-date');
 const {GraphQLJSON, GraphQLJSONObject} = require('graphql-type-json');
 
-const {Query} = require('./src/directives/query');
-const {Page} = require('./src/directives/page');
-const {Sort} = require('./src/directives/sort');
-const {Result, ResultResolver} = require('./src/directives/result');
-const {Insert, Update, Upsert, Remove, Mutation, makeInputDirective} = require('./src/directives/input');
-const {Batch} = require('./src/directives/batch');
-const {Auth} = require('./src/directives/auth');
-const {Jwt} = require('./src/directives/jwt');
-const {AugmentedArgResolver} = require('./src/tools');
-const {checkAuth, getJwtPayload, sidecar} = require('./src/utils');
-const config = require('./src/config');
+const {Query, makeQueryDirective} = require('./lib/directives/query');
+const {Insert, Update, Upsert, Remove, Mutation, makeMutationDirective} = require('./lib/directives/mutation');
+const {Jwt} = require('./lib/directives/jwt');
+const {AugmentedArgResolver} = require('./lib/resolver');
+const config = require('./lib/config');
 
 
 module.exports = {
+    GraphQLDateTime, GraphQLJSON, GraphQLJSONObject,
+    
     setConfig: (conf) => {
         Object.assign(config, conf);
     },
@@ -26,25 +22,16 @@ module.exports = {
             [config.MODE_UPSERT]: Upsert,
             [config.MODE_REMOVE]: Remove,
             [config.MODE_MUTATE]: Mutation,
-            page: Page,
-            sort: Sort,
-            result: Result,
-            batch: Batch,
-            auth: Auth,
             jwt: Jwt,
         };
+        for (const mode of config.EXTRA_QUERY_MODES) {
+            directives[mode] = makeQueryDirective(mode);
+        }
         for (const mode of config.EXTRA_MUTATE_MODES) {
-            directives[mode] = makeInputDirective(mode);
+            directives[mode] = makeMutationDirective(mode);
         }
         return directives;
     },
 
     AugmentedArgResolver,
-    ResultResolver,
-
-    checkAuth,
-    getJwtPayload,
-    sidecar,
-
-    GraphQLDateTime, GraphQLJSON, GraphQLJSONObject,
 };
